cabae2a8de7084adc240be6f4ffa7213ee74b164
nrobeR <mr.nrober@gmail.com>
2014-08-26 01:52:59 -0700

    moved app & package to main folder

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/client'));
+
 app.get('/',function(req,res){
-  res.sendfile('../client/index.html');
+  res.sendfile('client/index.html');
 })
 
 app.listen(3000);
