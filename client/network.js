25c2b16ad02827a56117c7c10277e4886ab7730b
nrobeR <mr.nrober@gmail.com>
2014-08-25 23:29:52 -0700

    get graph work!

--- client/network.js
+++ client/network.js
@@ -1,6 +1,6 @@
 var width = 800;
 var height = 500;
-var radius = 5;
+var radius = 10;
 var node, link;
 
 var board = d3.select("body")
@@ -12,8 +12,18 @@ var randomGenerator = function(n){
   return Math.floor(Math.random()*n);
 };
 
-var randomX = function(){return randomGenerator(width-radius*2);}
-var randomY = function(){return randomGenerator((height-radius*2))}
+var randomX = function(){return randomGenerator(width-radius*2);};
+var randomY = function(){return randomGenerator((height-radius*2))};
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+              .charge(-150)
+              .linkDistance(80)
+              .size([width,height]);
+
+force.nodes(TestData.nodes)
+     .links(TestData.links)
+     .start();
 
 var friends = board.selectAll(".friends")
                    .data(TestData.nodes)
@@ -25,6 +35,25 @@ var friends = board.selectAll(".friends")
                    	cy: randomY,
                    	r: radius
                    })
-                   .style("fill","#E05723");
+                   .style("fill",function(d){return color(d.group)})
+                   .call(force.drag);
 
+var links = board.selectAll(".links")
+                 .data(TestData.links)
+                 .enter()
+                 .append("line")
+                 .attr("class","links")
+                 .style("stroke-width",function(d){return Math.sqrt(d.value);});
+                 // .style("stroke-width",5);
+
+force.on("tick",function(){
+  links.attr("x1",function(d){return d.source.x;})
+      .attr("y1",function(d){return d.source.y;})
+      .attr("x2",function(d){return d.target.x;})
+      .attr("y2",function(d){return d.target.y;});
+
+  friends.attr("cx",function(d){return d.x})
+         .attr("cy",function(d){return d.y});
+
+})
 
